5e296688f048cebdc013b6327e4163ecf8643e92
Leaofy <=>
2022-04-06 21:57:23 -0300

    feat: index file created

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-import 'dotenv/config'
-import { Client, Intents } from 'discord.js'
-
-const client = new Client({ intents: 
-  [ Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES 
-  ]
-})
-
-client.login(process.env.ACCESS_TOKEN).then(console.log)
+export * from './handlers/eventInterface'
