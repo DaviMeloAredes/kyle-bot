f06987c92aab5262d7a6bfda2198d57b3f1ed196
Leaofy <=>
2022-04-07 16:54:02 -0300

    feat: db's start-up added

--- src/setup.ts
+++ src/setup.ts
@@ -4,6 +4,7 @@ import { Client, Intents } from 'discord.js'
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 (async () => {
+  (await import('./db/handlers/startUpDbHandler')).default();
   (await import('./handlers/eventHandler')).default(client)
 })()
 
