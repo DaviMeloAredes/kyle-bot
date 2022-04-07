9172444e3f693a0a07d2b062702db2e24044f80f
Leaofy <=>
2022-04-07 16:56:36 -0300

    feat: server validation added

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,10 +1,21 @@
 import { Message } from 'discord.js'
+import { ServerModel } from '../db/models/serverModel'
 
 export default {
   name: 'messageCreate',
   exec: async (message: Message) => {
-    if (message.author.bot) return;
+    if (message.author.bot) return
 
-    (await import('../handlers/commandHandler')).default(message)
+    const server = await ServerModel.findOne({ sv_id: message.guild!.id })
+
+    if (!server) {
+      return (await import('../db/handlers/addServerHandler')).default(message).then(async () => {
+        const createdServer = await ServerModel.findOne({ sv_id: message.guild!.id })
+
+        return (await import('../handlers/commandHandler')).default(message, createdServer.prefix)
+      })
+    }
+
+    (await import('../handlers/commandHandler')).default(message, server.prefix)
   }
 }
