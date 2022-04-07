b752338502bf2736cb8a108ccbc7e49f2d351fbd
Leaofy <=>
2022-04-07 11:46:32 -0300

    style: fixed code to comply with eslint

--- src/setup.ts
+++ src/setup.ts
@@ -1,11 +1,7 @@
 import 'dotenv/config'
 import { Client, Intents } from 'discord.js'
 
-const client = new Client({ intents: 
-  [ Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES 
-  ]
-});
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 (async () => {
   (await import('./handlers/eventHandler')).default(client)
