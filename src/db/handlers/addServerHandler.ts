ee879ab45ce167d78a913694e08ab23ecc84a9b0
Leaofy <=>
2022-04-07 16:54:43 -0300

    feat: 'prefix' property added to server schema

--- src/db/handlers/addServerHandler.ts
+++ src/db/handlers/addServerHandler.ts
@@ -1,11 +1,15 @@
 import { Message } from 'discord.js'
 import { ServerModel } from '../models/serverModel'
 
-export default async (message: Message, cb?: any) => {
+export default async (message: Message) => {
   const servers = await ServerModel.find({})
+
+  console.log(servers)
+
   if (!servers.some(server => server.id === message.guild!.id)) {
     const serverDoc = await new ServerModel({
-      id: message.guild!.id,
+      sv_id: message.guild!.id,
+      prefix: '=',
       users: [
         {
           id: message.author.id
