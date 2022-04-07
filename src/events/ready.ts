0007333be84954b57d818a2ba250765948d089e2
Leaofy <=>
2022-04-06 22:11:41 -0300

    feat: 'ready' event added

---
+++ src/events/ready.ts
@@ -0,0 +1,8 @@
+import { Client } from "discord.js";
+
+export default {
+  name: 'ready',
+  exec: (bot: Client) => {
+    console.log(`I'm online in the robotic body of ${bot.user!.username}!`)
+  }
+}
