10094289cc3a744af1ee974ec48be3d6650109c4
Leaofy <=>
2022-04-07 16:56:01 -0300

    feat: server validation added

--- src/handlers/commandHandler.ts
+++ src/handlers/commandHandler.ts
@@ -2,14 +2,14 @@ import fG from 'fast-glob'
 import { Message } from 'discord.js'
 import { CommandInterface } from '../index'
 
-export default async (message: Message) => {
+export default async (message: Message, prefix: string) => {
   const commandFile = await fG('src/commands/**/**')
 
   commandFile.forEach(async (command: string) => {
     const dir = `../../${command}`
     const commandProps: CommandInterface = (await import(dir)).default
 
-    if (message.content.toUpperCase() === commandProps.name.toUpperCase()) {
+    if (message.content.toUpperCase().startsWith(`${prefix}${commandProps.name.toUpperCase()}`)) {
       commandProps.exec(message)
     }
   })
